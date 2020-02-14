bfc77101af929206aea2ced6d3458331c5013ece
huiminsuper <huimin.super@qq.com>
2020-02-14 14:06:44 +0800

    add subMenuOne table

--- app/src/pages/list.jsx
+++ app/src/pages/list.jsx
@@ -2,33 +2,35 @@ import React from 'react';
 import { Title } from '../components/Title'
 import { Table, Tooltip, Icon } from 'antd';
 import api from '../api/index-client';
+import Mock from 'mockjs';
 
 let debug = 1
-// if(debug){
-
-// }
+if(debug){
+      Mock.mock('/getTable/Data',{
+            "data|121-140": [{
+                "key|+1":1,
+                "name":"@name",
+                "sex|1":["男","女"],
+                "age":"@integer(22, 60)",
+                "email":"@email"
+            }]
+        }) 
+}
 
 export default class List extends React.Component {
       constructor(props){
             super(props);
+            this.state = { data: [] }
       }
       async getData (){
             const resp = await api.get('/getTable/Data');
             console.log('resp :', resp);
+           if(resp.status === 200){ this.setState({data: resp.data.data}) }
       }
       componentDidMount(){
             this.getData();
       }
       render(){
-            Mock.mock(/getTableData/,{
-                  "data|121-140": [{
-                      "key|+1":1,
-                      "name":"@name",
-                      "sex|1":["男","女"],
-                      "age":"@integer(22, 60)",
-                      "email":"@email"
-                  }]
-              }) 
             const columns = [
                   { title: '姓名', dataIndex: 'name' },
                   { title: '性别', dataIndex: 'sex' },
@@ -42,10 +44,27 @@ export default class List extends React.Component {
                         </span>
                   ) }
             ];
+            const pagination = { // 待分析
+                  size:"default",
+                  showQuickJumper:true,
+                  total: this.state.data.length,
+                  showSizeChanger: true,
+                  onShowSizeChange: (current, pageSize) => {
+                        console.log('Current: ', current, '; PageSize: ', pageSize);
+                  },
+                  onChange: (current) => {
+                        console.log('Current: ', current);
+                  },
+            }
             return(
                   <div>
                         { Title("Android 子导航一(表格)") }
-                        <Table columns={columns} dataSource={data} />
+                        <Table 
+                              size="small" 
+                              columns={columns} 
+                              dataSource={this.state.data}
+                              pagination={pagination}
+                        />
                   </div>
             )
       }
